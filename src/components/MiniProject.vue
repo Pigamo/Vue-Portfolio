e43724daf206159636bf06029049e315aafda7a4
Pigamo <chosongames@gmail.com>
2023-05-16 19:40:51 -0400

    Xfer

---
+++ src/components/MiniProject.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { getVid } from '~/content/videos'
+
+interface Project {
+  title: string
+  text: string
+  langs: { name: string; color: string }[]
+  vid: string
+  mainImage: string
+  url: string
+
+}
+const { project, vid } = defineProps<{ project: Project; vid: string }>()
+
+const target = ref(null)
+
+const { x, y, isOutside } = useMouseInElement(target)
+const video = ref<string>('')
+
+async function getVideo(n: string) {
+  const v = await getVid(n)
+  video.value = v
+}
+
+const videoUrls = ref<Map<string, string>>(new Map())
+
+async function getVideoUrl(videoName: string) {
+  if (videoUrls.value.has(videoName)) {
+    return videoUrls.value.get(videoName)
+  }
+  else {
+    const videoUrl = await getVid(videoName)
+    videoUrls.value.set(videoName, videoUrl)
+    return videoUrl
+  }
+}
+const vidControls = ref(null)
+onMounted(async () => {
+  if (project.vid === null || project.vid === undefined || project.vid === '')
+    return
+
+  video.value = await getVideoUrl(project.vid)
+})
+const vis = useMouseInElement(target)
+watchEffect(() => {
+  if (vidControls.value === null)
+    return
+  if (!vis.isOutside.value)
+    vidControls.value.play()
+  else if (vis.isOutside.value)
+    vidControls.value.pause()
+})
+</script>
+
+<template>
+  <div
+
+    relative min-h-80 w-full flex cursor-pointer items-center justify-center overflow-hidden shadow-md :class="video.length > 0 ? '  col-span-5' : 'col-span-5'"
+  >
+    <div ref="target" relative z-2 h-full w-full>
+      <div h-full w-full flex items-center justify-start p5 text-6xl font-bold text-light transition-opacity duration-500 ease-in-out hover:opacity-0 class="bg-dark/50">
+        {{ project.title }}
+      </div>
+    </div>
+    <video
+      v-if="video.length > 0"
+      ref="vidControls" :src="`${video}#t=12`"
+      :autoplay="!vis.isOutside" class="-z-1" loop muted playsinline object-fit absolute left-0 top-0 h-full w-full object-cover
+    />
+    <img
+      v-else-if="project.mainImage"
+      :src="project.mainImage"
+      object-fit absolute left-0 class="-z-1" top-0 h-full w-full object-cover
+    >
+  </div>
+</template>
---
+++ src/components/ProjectOverview.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Project {
+  title: string
+  text: string
+  langs: { name: string; color: string }[]
+  vid: string
+  mainImage: string
+  url: string
+
+}
+const { projects } = defineProps<{ projects: Project[] }>()
+</script>
+
+<template>
+  <div grid grid-flow-row-dense grid-cols-12 w-full gap-10>
+    <MiniProject v-for="project in projects" :key="project" class="holder" :project="project" />
+  </div>
+</template>
